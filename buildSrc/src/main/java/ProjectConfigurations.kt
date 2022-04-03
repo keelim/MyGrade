161c76444e6594c2d80c8acd4929ba0f1adc5a1b
keelim <kimh00335@gmail.com>
2022-04-04 00:19:31 +0900

    [VERSION] VERSION UP 0.0.7

--- buildSrc/src/main/java/ProjectConfigurations.kt
+++ buildSrc/src/main/java/ProjectConfigurations.kt
@@ -6,7 +6,7 @@ object ProjectConfigurations {
     const val targetSdk = 31
     const val buildTools = "31.0.0"
     const val applicationID = "com.keelim.mygrade"
-    const val versionCode = 6
-    const val versionName = "0.0.6"
+    const val versionCode = 7
+    const val versionName = "0.0.7"
     val javaVer = JavaVersion.VERSION_11
 }
